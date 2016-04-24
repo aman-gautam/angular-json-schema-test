335478b702c291615cef69ed0ff65c521b1dc021
Aman Gautam <ipians.aman@gmail.com>
2016-04-24 14:59:49 +0530

    Adding date picker

--- scripts/app.js
+++ scripts/app.js
@@ -7,3 +7,30 @@ angular.module('app', ['ngRoute', 'schemaForm'])
             })
     });
 
+angular.module('app').config(function(schemaFormDecoratorsProvider, sfBuilderProvider) {
+
+    schemaFormDecoratorsProvider.defineAddOn(
+        'bootstrapDecorator',         // Name of the decorator you want to add to.
+        'awesome',                    // Form type that should render this add-on
+        'templates/my/addon.html',    // Template name in $templateCache
+        sfBuilderProvider.stdBuilders // List of builder functions to apply.
+    );
+
+});
+
+angular.module('app').config(function(schemaFormDecoratorsProvider, sfBuilderProvider) {
+    var base = 'views/decorators/';
+    schemaFormDecoratorsProvider.defineDecorator('awesomeDecorator', {
+        textarea: {template: base + 'textarea.html', builder: sfBuilderProvider.stdBuilders},
+        button: {template: base + 'submit.html', builder: sfBuilderProvider.stdBuilders},
+        text: {template: base + 'text.html', builder: sfBuilderProvider.stdBuilders},
+        aman: {template: base + 'aman.html', builder: sfBuilderProvider.stdBuilders},
+
+        // The default is special, if the builder can't find a match it uses the default template.
+        'default': {template: base + 'default.html', builder: sfBuilderProvider.stdBuilders}
+    }, []);
+});
+
+angular.module('app').config(['schemaFormDecoratorsProvider', function(decoratorsProvider){
+    decoratorsProvider.addMapping('bootstrapDecorator', 'string', '/views/decorators/ckeditor.html');
+}]);
--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -3,18 +3,31 @@ angular.module('app')
         console.log('MainCtrl');
 
         // This can go to the resolve, but let it stay here for sake of simplicity...
-        $http.get('/schemas/form.json').then(function(res){
-           $scope.schema = {
-               type: "object",
-               properties: res.data
-           }
-        }, function(err){
-            console.log(err);
-        });
+        //$http.get('/schemas/form.json').then(function(res){
+        //   $scope.schema = {
+        //       type: "object",
+        //       properties: res.data
+        //   }
+        //}, function(err){
+        //    console.log(err);
+        //});
 
         $scope.schema = {
             type: "object",
             properties: {
+                "name": { "type": "string", "minLength": 2, "title": "Name", "description": "Name or alias" },
+                "title": {
+                    "type": "string",
+                    "enum": ["dr","jr","sir","mrs","mr","dj"]
+                },
+                "deleted": {
+                    "type": "boolean"
+                },
+                "birthDate": {
+                    "title": "Bday",
+                    "type": "string",
+                    "format": "date"
+                }
             }
         };
 
